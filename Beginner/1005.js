5b6335d003f6a371434fd7cfc995af289c632dcc
Shannon Mettry <shannonmettry12@gmail.com>
2022-04-23 17:37:22 +0200

    finished up to 1006

--- Beginner/1005.js
+++ Beginner/1005.js
@@ -1,9 +1,12 @@
 const input = require("fs").readFileSync("/dev/stdin", "utf8").trim();
 
-let A = parseFloat(lines.shift());
-let B = parseFloat(lines.shift());
+let value = input.split("\n");
 
-let media = (A * 3.5 + B * 7.5) / 11;
-console.log(`MEDIA = ${media.toFixed(5)}`);
+let a = parseFloat(value.shift());
+let b = parseFloat(value.shift());
+
+let media = (a * 3.5 + b * 7.5) / 11.0;
+
+console.log("MEDIA = " + media.toFixed(5));
 
 //wrong
--- Beginner/1006.js
+++ Beginner/1006.js
@@ -1,9 +1,13 @@
 const input = require("fs").readFileSync("/dev/stdin", "utf8").trim();
 
-let A = parseFloat(lines.shift());
-let B = parseFloat(lines.shift());
-let C = parseFloat(lines.shift());
+let value = input.split("\n");
 
+let a = parseFloat(value.shift());
+let b = parseFloat(value.shift());
+let c = parseFloat(value.shift());
 
-let media = (A * 2 + B * 3 + C * 5) / 11;
-console.log(`MEDIA = ${media}`);
+let media = (a * 2 + b * 3 + c * 5) / 10.0;
+
+console.log("MEDIA = " + media.toFixed(1));
+
+//wrong
