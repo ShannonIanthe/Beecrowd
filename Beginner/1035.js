ece4e71a41caf1e9628ff0d015e2236197f54a48
Shannon Mettry <shannonmettry12@gmail.com>
2022-04-30 13:27:26 +0200

    1035

---
+++ Beginner/1035.js
@@ -0,0 +1,14 @@
+const input = require("fs").readFileSync("/dev/stdin", "utf8").trim();
+
+let value = input.split("\n");
+
+const a = parseInt(value.shift());
+const b = parseInt(value.shift());
+const c = parseInt(value.shift());
+const d = parseInt(value.shift());
+
+if (b > c && d > a && c + d > a + b && c > 0 && d > 0 && a % 2 == 0 ) {
+    greeting = "Valores aceitos";
+} else {
+    greeting = "Valores nao aceitos";
+}
