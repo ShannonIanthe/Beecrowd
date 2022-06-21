05e3f0dd113b684038d591d335c1c839382213ba
Shannon Mettry <shannonmettry12@gmail.com>
2022-06-21 14:34:59 +0200

    1038 attempt

--- Beginner/1038.js
+++ Beginner/1038.js
@@ -5,6 +5,9 @@ const arr = [0, 4.00, 4.50, 5.00, 2.00, 1.5];
 
 //first number is the price value in the index
 //second number is how many you had of the product
+function total(arr, index) {
+    return value(arr[i]) * value;
+}
 total = arr[i] * value;
 
 console.log(`Total: R$ ${total}`);
